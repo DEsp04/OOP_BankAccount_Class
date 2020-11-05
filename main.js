42fa56391919e56aa85e6c4274a419d7ccd9504a
david espinal <despinal0425@gmail.com>
2020-11-04 22:11:22 -0500

    completed lab

---
+++ main.js
@@ -0,0 +1,61 @@
+class BankAccount { 
+  constructor(type, money, backupAccount = null) { 
+    this.type = type;
+    this.money = money;
+    this.transactionHistory = [];
+    this.backupAccount = backupAccount;
+  }
+
+  withdraw(amount) { 
+    const previousBalance = this.money;
+    this.money = this.money - amount;
+
+    this.transactionHistory.push({
+      previousBalance: previousBalance,
+      transactionType: 'withdrawal',
+      transactionAmount: amount,
+      endingBalance: this.money
+    });
+
+    //change to positive amount 
+    if(this.money < 0) { 
+      const overdrawnAmount = this.money * -1;
+
+      //withdraw from savings (backup) account
+      this.backupAccount.withdraw(overdrawnAmount)
+
+      //deposit from savings to checking(this account)
+      this.deposit(overdrawnAmount);
+
+      console.log('overdraft!!!')
+    }
+  }
+
+
+
+  deposit(depo) { 
+    const previousBalance = this.money;
+    this.money = this.money + depo;
+
+    this.transactionHistory.push({
+      previousBalance: previousBalance,
+      transactionType: 'deposit',
+      transactionAmount: depo,
+      endingBalance: this.money
+    })
+  }
+
+  showBalance() { 
+    return this.money;
+  }
+
+}
+
+const savingsAccount = new BankAccount('savings', 1000);
+const checkingAccount = new BankAccount('checking', 0, savingsAccount);
+console.log(checkingAccount);
+console.log(savingsAccount);
+
+checkingAccount.withdraw(100);
+console.log(checkingAccount);
+console.log(savingsAccount);
